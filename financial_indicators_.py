ec2f39dc84696486457a465fc9ec708f1e7d0262
Daniel8B <118355633+Daniel8B@users.noreply.github.com>
2024-02-18 02:54:17 +0200

    Update financial_indicators_.py

--- financial_indicators_.py
+++ financial_indicators_.py
@@ -170,8 +170,8 @@ max_date = datetime.date(2024,1,1)
 
 # Sidebar inputs
 ticker = st.sidebar.text_input('Ticker: ')
-start_date = st.sidebar.date_input('Start Date', min_value=min_date1, max_value=max_date)
-end_date = st.sidebar.date_input('End Date', min_value=min_date2, max_value=today)
+start_date = st.sidebar.date_input('Start Date', min_value= datetime.date(2000,1,1), max_value= datetime.date(2024,1,1))
+end_date = st.sidebar.date_input('End Date', min_value=datetime.date(2001,1,1), max_value=today)
 
 # Download data
 load_data_button = st.button("Load Data")
